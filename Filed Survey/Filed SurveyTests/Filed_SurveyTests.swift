452e3a34bd0a04408204289dab88d9ec7383b986
来思良 <laisiliang@laisiliangdembp.centurylink.net>
2017-11-03 16:46:01 -0500

    Start of Field Survey project

---
+++ Filed Survey/Filed SurveyTests/Filed_SurveyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Filed_SurveyTests.swift
+//  Filed SurveyTests
+//
+//  Created by 来思良 on 2017/11/3.
+//  Copyright © 2017年 Yuzhou He. All rights reserved.
+//
+
+import XCTest
+@testable import Filed_Survey
+
+class Filed_SurveyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
